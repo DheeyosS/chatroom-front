d9c23047d3101dbb08189e366fe75d4c3bf8e802
Dheeyos <manutd.leo@hotmail.com>
2020-11-08 00:22:10 +0700

    add some styling

--- src/Posts.js
+++ src/Posts.js
@@ -1,31 +1,50 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Posts extends Component {
-    state = {
-        posts: [],
-    };
+  state = {
+    posts: [],
+  };
 
-    componentDidMount(){
-        axios.get("http://127.0.0.1:8000/api/posts/",{
-            headers: {
-                Authorization: `Token 311067c9572651952f190bd870aa2bc2cd55864c`,
-            },
-        }).then(res =>{
-           console.log(res.data);
-           this.setState({posts: res.data}); 
-        }); 
-    }
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:8000/api/posts/", {
+        headers: {
+          Authorization: `Token 311067c9572651952f190bd870aa2bc2cd55864c`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ posts: res.data });
+      });
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>All Posts</h1>
-        <ul>
-          {this.state.posts.map((post,i) => <li key={'${post.id} ${i}'}>{post.text}</li>)}
-        </ul> 
+        <ul className="postlist">
+          {this.state.posts.map((post, i) => (
+            <li key={post.id}>
+              {post.username}
+              <br />
+              {post.text}
+              <br />
+              <ul className="commentlist">
+                {post.comments.map((comment, j) => (
+                  <li>
+                    {" "}
+                    {comment.username}
+                    <br />
+                    {comment.text}{" "}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-export default Posts
+export default Posts;
